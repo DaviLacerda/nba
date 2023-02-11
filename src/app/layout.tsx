7227fc81ae2fc3fb87bced56dc15902bb1ed56bc
Davi Lacerda <dlacerda9@gmail.com>
2023-02-11 16:07:10 -0300

    docs: Setup config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
 import './globals.css'
 
+export const metadata = {
+  title: 'NBA',
+  description: 'Dashboard for NBA Scores and Stats',
+  keywords: 'NBA, Scores, Stats, Dashboard',
+  icons: {
+    icon: '/favicon.png',
+  }
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -12,7 +21,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className='bg-zinc-900 text-zinc-100'>{children}</body>
     </html>
   )
 }
