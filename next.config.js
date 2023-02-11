5a69e69837dd887dea8045a53a672691d9ef84d4
Davi Lacerda <dlacerda9@gmail.com>
2023-02-11 16:31:52 -0300

    build: next config and new packages

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    // Example: https://a.espncdn.com/i/teamlogos/nba/500/bos.png
+    // Default: https://a.espncdn.com/i/teamlogos/default-team-logo-500.png
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'a.espncdn.com',
+        port: '',
+        pathname: '/i/teamlogos/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
